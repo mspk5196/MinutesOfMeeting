aa3e8ecd7e3264d0f1eee5e4a462da56c88e98ca
Sanjeevi Rajan <>
2025-12-23 16:19:16 +0530

    Port

--- backend/b2.js
+++ backend/b2.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: true,
     credentials: true
 }));
 
