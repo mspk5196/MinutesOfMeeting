1a3e3371b971b73017fe3fc14b0e4b3885000cb4
PRANESH KARTHI M S <107921577+mspk5196@users.noreply.github.com>
2025-12-26 10:42:09 +0530

    commented logs

--- backend/b2.js
+++ backend/b2.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: true,
+    origin: 'https://pcdp.bitsathy.ac.in',
     credentials: true
 }));
 
