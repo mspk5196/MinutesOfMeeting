ae12b838d27138582e37cfb8e543e1c3d0b9d59f
PRANESH KARTHI M S <107921577+mspk5196@users.noreply.github.com>
2025-12-30 10:49:21 +0530

    Update Dockerfile and docker-compose.yml to change exposed ports and health check endpoint

--- backend/b2.js
+++ backend/b2.js
@@ -56,6 +56,6 @@ app.use((err, req, res, next) => {
 initScheduler();
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
